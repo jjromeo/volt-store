548ef6b7a1a16c82e1735796f9479df3a80b3da8
Jerome Pratt <jerome.pratt@outlook.com>
2015-02-15 12:26:15 +0000

    changed alerts on adding products to be flash notices, and added alert indicating whether a product has been added

--- app/main/controllers/products_controller.rb
+++ app/main/controllers/products_controller.rb
@@ -12,8 +12,12 @@ class ProductsController < Volt::ModelController
   end
 
   def add_product
-    _products << {name: page._new_product._name, price: page._new_product._price}
-    page._product_added = true
+    store._products << {name: page._new_product._name, price: page._new_product._price}
+    if page._new_product._name.length > 4
+      flash._successes << 'Your product has been added'
+    else
+      flash._errors << 'Product name must be higher than 4 characters, please try again'
+    end
   end
 
   private
